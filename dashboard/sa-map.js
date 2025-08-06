470d514bc99a6cb416beda6c681bb0dae5f47823
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-08-06 14:36:56 +0200

    Create sa-map.js

---
+++ dashboard/sa-map.js
@@ -0,0 +1,104 @@
+// South Africa Province GeoJSON
+const southAfricaGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {"name": "Gauteng", "code": "ZA-GT"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [27.5, -25.5], [28.5, -25.5], [28.5, -26.5], [27.5, -26.5], [27.5, -25.5]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Western Cape", "code": "ZA-WC"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [18.0, -31.0], [19.0, -31.0], [19.0, -32.0], [20.0, -33.0], [21.0, -34.0],
+                    [22.0, -34.5], [23.0, -34.0], [23.5, -33.5], [18.0, -33.5], [18.0, -31.0]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "KwaZulu-Natal", "code": "ZA-KZN"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [29.0, -27.0], [30.5, -27.0], [31.5, -27.5], [32.5, -28.0], [32.5, -30.0],
+                    [31.0, -30.5], [30.0, -30.0], [29.0, -29.0], [29.0, -27.0]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Eastern Cape", "code": "ZA-EC"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [24.0, -31.0], [25.0, -31.5], [26.0, -32.0], [27.0, -33.0], [28.0, -33.5],
+                    [29.0, -33.0], [29.0, -31.0], [27.0, -30.0], [25.0, -30.0], [24.0, -31.0]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Free State", "code": "ZA-FS"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [25.0, -27.0], [26.5, -27.0], [27.5, -28.0], [28.5, -29.0], [29.0, -30.0],
+                    [27.0, -30.0], [25.0, -29.0], [25.0, -27.0]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Mpumalanga", "code": "ZA-MP"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [29.0, -24.5], [30.5, -25.0], [31.0, -26.0], [30.0, -27.0], [29.0, -27.0],
+                    [28.0, -26.0], [28.0, -25.0], [29.0, -24.5]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Limpopo", "code": "ZA-LP"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [27.5, -22.0], [29.0, -22.5], [30.5, -23.0], [31.5, -24.0], [31.0, -25.0],
+                    [29.0, -24.5], [27.5, -24.0], [27.5, -22.0]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "North West", "code": "ZA-NW"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [24.0, -25.5], [25.5, -25.5], [26.5, -26.5], [27.5, -27.0], [27.0, -27.5],
+                    [25.0, -27.5], [24.0, -26.5], [24.0, -25.5]
+                ]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"name": "Northern Cape", "code": "ZA-NC"},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [[
+                    [18.0, -26.0], [20.0, -26.5], [22.0, -27.0], [24.0, -27.5], [25.0, -28.5],
+                    [24.0, -29.5], [22.0, -30.0], [20.0, -30.5], [18.0, -30.5], [18.0, -26.0]
+                ]]
+            }
+        }
+    ]
+};
